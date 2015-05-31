e845117c601a40c37eedd69c946bb41658726900
tumluliu <nudtlliu@gmail.com>
2015-05-31 13:25:00 +0800

    fix the hardcoding in vertex_id creation, change osm_point to street_junctions

--- gen_car-parking_switch_points.sql
+++ gen_car-parking_switch_points.sql
@@ -1,10 +1,10 @@
 --for parking places
 ALTER TABLE switch_points DROP CONSTRAINT switch_points_pkey;
 INSERT INTO switch_points (switch_point_id, from_vertex_id, to_vertex_id, from_mode_id, to_mode_id, type_id, cost, is_available, ref_poi_id)
-SELECT 0, 1100000000+g1.nn_gid, 1200000000+g2.nn_gid, 11, 12, 91, 3, 't', g1.osm_id FROM
-(SELECT c.osm_id, (pgis_fn_nn(c.geom, 0.002, 1, 10, '(SELECT * FROM osm_point INNER JOIN vertices ON osm_point.osm_id=(vertices.vertex_id % 100000000) WHERE vertices.mode_id=11)', 'true', 'osm_id', 'way')).* FROM (SELECT * FROM car_parkings) c) AS g1,
-(SELECT c.osm_id, (pgis_fn_nn(c.geom, 0.002, 1, 10, '(SELECT * FROM osm_point INNER JOIN vertices ON osm_point.osm_id=(vertices.vertex_id % 100000000) WHERE vertices.mode_id=12)', 'true', 'osm_id', 'way')).* FROM (SELECT * FROM car_parkings) c) AS g2
+SELECT 0, (11 || g1.nn_gid::text)::bigint, (12 || g2.nn_gid::text)::bigint, 11, 12, 91, 3, 't', g1.osm_id FROM
+(SELECT c.osm_id, (pgis_fn_nn(c.geom, 0.002, 1, 10, '(SELECT * FROM street_junctions INNER JOIN vertices ON street_junctions.osm_id=vertices.raw_point_id WHERE vertices.mode_id=11)', 'true', 'osm_id', 'geom')).* FROM (SELECT * FROM car_parkings) c) AS g1,
+(SELECT c.osm_id, (pgis_fn_nn(c.geom, 0.002, 1, 10, '(SELECT * FROM street_junctions INNER JOIN vertices ON street_junctions.osm_id=vertices.raw_point_id WHERE vertices.mode_id=12)', 'true', 'osm_id', 'geom')).* FROM (SELECT * FROM car_parkings) c) AS g2
 WHERE g1.osm_id=g2.osm_id;
-UPDATE switch_points SET switch_point_id=9100000000+id WHERE switch_point_id=0;
+UPDATE switch_points SET switch_point_id=(91 || id::text)::bigint WHERE switch_point_id=0;
 ALTER TABLE switch_points ADD CONSTRAINT switch_points_pkey PRIMARY KEY (switch_point_id);
 
--- gen_s-bahn-station_switch_points.sql
+++ gen_s-bahn-station_switch_points.sql
@@ -2,16 +2,16 @@
 --from s-bahn stations to pedestrian network
 ALTER TABLE switch_points DROP CONSTRAINT switch_points_pkey;
 INSERT INTO switch_points (switch_point_id, from_vertex_id, to_vertex_id, from_mode_id, to_mode_id, type_id, cost, is_available, ref_poi_id)
-SELECT 0, 1400000000+g1.nn_gid, 1200000000+g2.nn_gid, 14, 12, 95, 0, 't', g1.type_id FROM
+SELECT 0, (14 || g1.nn_gid::text)::bigint, (12 || g2.nn_gid::text)::bigint, 14, 12, 95, 0, 't', g1.type_id FROM
 (SELECT c.type_id, (pgis_fn_nn(c.geom, 0.002, 1, 10, '(SELECT * FROM suburban_junctions INNER JOIN vertices ON suburban_junctions.nodeid=(vertices.vertex_id % 100000000) WHERE vertices.mode_id=14)', 'true', 'nodeid', 'geom')).* FROM (SELECT * FROM suburban_stations) c) AS g1,
-(SELECT c.type_id, (pgis_fn_nn(c.geom, 0.002, 1, 10, '(SELECT * FROM osm_point INNER JOIN vertices ON osm_point.osm_id=vertices.raw_point_id WHERE vertices.mode_id=12)', 'true', 'osm_id', 'way')).* FROM (SELECT * FROM suburban_stations) c) AS g2
+(SELECT c.type_id, (pgis_fn_nn(c.geom, 0.002, 1, 10, '(SELECT * FROM street_junctions INNER JOIN vertices ON street_junctions.osm_id=vertices.raw_point_id WHERE vertices.mode_id=12)', 'true', 'osm_id', 'geom')).* FROM (SELECT * FROM suburban_stations) c) AS g2
 WHERE g1.type_id=g2.type_id;
 --from pedestrian network to s-bahn
 INSERT INTO switch_points (switch_point_id, from_vertex_id, to_vertex_id, from_mode_id, to_mode_id, type_id, cost, is_available, ref_poi_id)
-SELECT 0, 1200000000+g1.nn_gid, 1400000000+g2.nn_gid, 12, 14, 95, 5, 't', g1.type_id FROM
-(SELECT c.type_id, (pgis_fn_nn(c.geom, 0.002, 1, 10, '(SELECT * FROM osm_point INNER JOIN vertices ON osm_point.osm_id=vertices.raw_point_id WHERE vertices.mode_id=12)', 'true', 'osm_id', 'way')).* FROM (SELECT * FROM suburban_stations) c) AS g1,
+SELECT 0, (12 || g1.nn_gid::text)::bigint, (14 || g2.nn_gid::text)::bigint, 12, 14, 95, 5, 't', g1.type_id FROM
+(SELECT c.type_id, (pgis_fn_nn(c.geom, 0.002, 1, 10, '(SELECT * FROM street_junctions INNER JOIN vertices ON street_junctions.osm_id=vertices.raw_point_id WHERE vertices.mode_id=12)', 'true', 'osm_id', 'geom')).* FROM (SELECT * FROM suburban_stations) c) AS g1,
 (SELECT c.type_id, (pgis_fn_nn(c.geom, 0.002, 1, 10, '(SELECT * FROM suburban_junctions INNER JOIN vertices ON suburban_junctions.nodeid=(vertices.vertex_id % 100000000) WHERE vertices.mode_id=14)', 'true', 'nodeid', 'geom')).* FROM (SELECT * FROM suburban_stations) c) AS g2
 WHERE g1.type_id=g2.type_id;
-UPDATE switch_points SET switch_point_id=9500000000+id WHERE switch_point_id=0;
+UPDATE switch_points SET switch_point_id=(95 || id::text)::bigint WHERE switch_point_id=0;
 ALTER TABLE switch_points ADD CONSTRAINT switch_points_pkey PRIMARY KEY (switch_point_id);
 
