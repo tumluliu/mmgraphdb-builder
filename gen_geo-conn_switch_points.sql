c4d30b0299e1fb4a6e3fa7db26eb90bf7d0200ee
tumluliu <nudtlliu@gmail.com>
2015-06-07 11:41:22 +0800

    fix typo

--- gen_geo-conn_switch_points.sql
+++ gen_geo-conn_switch_points.sql
@@ -5,6 +5,6 @@ SELECT 0, g1.vertex_id, g2.vertex_id, 11, 12, 92, 0.5, 't', NULL FROM
 (SELECT raw_point_id, vertex_id FROM vertices WHERE mode_id=11) AS g1,
 (SELECT raw_point_id, vertex_id FROM vertices WHERE mode_id=12) AS g2
 WHERE g1.raw_point_id=g2.raw_point_id;
-UPDATE switch_points SET switch_point_id=(92 || id::string)::bigint WHERE switch_point_id=0;
+UPDATE switch_points SET switch_point_id=(92 || id::text)::bigint WHERE switch_point_id=0;
 ALTER TABLE switch_points ADD CONSTRAINT switch_points_pkey PRIMARY KEY (switch_point_id);
 
