29fd8d30d50b0e28a89f81e82533bb88621540c7
tumluliu <nudtlliu@gmail.com>
2015-06-07 13:48:11 +0800

    rename file

--- export_public_transit_graph_from_um_db.sql
+++
@@ -1,2 +0,0 @@
-\copy (select length,speed_factor,mode_id,from_id,to_id,edge_id, null as osm_id from edges where mode_id != 1001 and mode_id != 1002) to 'public_transit_edges.csv' with csv header;
-\copy (select out_degree as outdegree,vertex_id,null as osm_id,mode_id,x as lon,y as lat from vertices where mode_id != 1001 and mode_id != 1002) to 'public_transit_vertices.csv' with csv header;
