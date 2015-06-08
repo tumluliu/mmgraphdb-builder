ff9c5f586766ee6c8ea1479ecea46a6461443866
tumluliu <nudtlliu@gmail.com>
2015-06-09 00:30:51 +0800

    use streetjunction osm_id as ref_poi_id of geo_connection type switch points

--- gen_geo-conn_switch_points.sql
+++ gen_geo-conn_switch_points.sql
@@ -1,7 +1,7 @@
 --for physical connections
 ALTER TABLE switch_points DROP CONSTRAINT switch_points_pkey;
 INSERT INTO switch_points (switch_point_id, from_vertex_id, to_vertex_id, from_mode_id, to_mode_id, type_id, cost, is_available, ref_poi_id)
-SELECT 0, g1.vertex_id, g2.vertex_id, 11, 12, 92, 0.5, 't', NULL FROM
+SELECT 0, g1.vertex_id, g2.vertex_id, 11, 12, 92, 0.5, 't', g1.raw_point_id FROM
 (SELECT raw_point_id, vertex_id FROM vertices WHERE mode_id=11) AS g1,
 (SELECT raw_point_id, vertex_id FROM vertices WHERE mode_id=12) AS g2
 WHERE g1.raw_point_id=g2.raw_point_id;
